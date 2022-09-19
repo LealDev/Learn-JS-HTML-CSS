8b70759180f5b26cc46098743fb97837de9d3313
LealDev <lealmaster0111@gmail.com>
2022-09-19 19:15:33 -0300

    azul

--- Java-Script/hscode/index.js
+++ Java-Script/hscode/index.js
@@ -1,1 +1,10 @@
 let cor = "azul"
+
+if (cor === "verde"){
+    console.log("siga");
+} else if (cor === "amarelo") {
+    console.log("siga");
+} else if (cor === "vermelho") {
+    console.log("pare");
+    
+}
